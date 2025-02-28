eece844e7a9a95d05cd0681ec3c57031f899f430
ShraddhaAbhang <shraddhasampatabhang@gmail.com>
2025-02-28 23:33:27 +0530

    Added PCA, Feature Sselection and BERT key concept with easy explanantion

---
+++ concept.py
@@ -0,0 +1,25 @@
+'''
+Deep Dive into Machine Learning and Deep Learning: Key Terms and Real-World Applications
+
+Explore the fundamentals and advanced concepts of machine learning and deep learning in this 
+comprehensive guide. From algorithms to evaluation metrics, we cover it all, providing clear explanations 
+and real-world examples to help you understand these complex topics. Whether you’re new to the field or looking 
+to deepen your expertise, this blog has something for everyone interested in AI and its applications.
+
+
+1. PCA (Principal Component Analysis): 
+→ PCA is a dimensionality reduction technique, PCA transforms the data into a set of orthogonal components that 
+capture the most variance in the data, making it easier to visualize and process.if we have many similar features 
+pca can be used to to reduce dimensionality and have fewer features which represents the all those features.
+
+2. Feature Selection: →
+→ feature selection is the process of selecting a subset of relevant features for use in a model, and getting rid 
+of noise in data using Correlation matrix technique.
+
+3. BERT: →
+→ BERT (Bidirectional Encoder Representations from Transformers) is a transformer-based model designed for natural 
+language processing (NLP) tasks. BERT is pre-trained on a large corpus of text in a bidirectional manner, meaning 
+it considers the context from both left and right directions. This allows it to understand the nuanced meaning of 
+words in context. BERT is used in search engines to better understand user queries and provide more relevant results.
+
+'''
